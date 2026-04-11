9cdc25a210d119e3e638c5104ae92d3fd887b53a
Sebas-1111 <js.martinezg1@uniandes.edu.co>
2026-04-11 17:03:19 -0500

    Avance clases: inventario prestamo

--- Proyecto-1-DPOO/src/Roles/InventarioPrestamo.java
+++ Proyecto-1-DPOO/src/Roles/InventarioPrestamo.java
@@ -1,4 +1,22 @@
+import java.util.List;
 
 public class InventarioPrestamo {
 
+	//Atributos
+	private List<String> listaJuegos;
+	
+	//Constructor
+	public InventarioPrestamo(List<String> listaJuegos) {
+		super();
+		this.listaJuegos = listaJuegos;
+	}
+
+	//Métodos
+	public List<String> getListaJuegos() {
+		return listaJuegos;
+	}
+
+	public void setListaJuegos(List<String> listaJuegos) {
+		this.listaJuegos = listaJuegos;
+	}
 }
