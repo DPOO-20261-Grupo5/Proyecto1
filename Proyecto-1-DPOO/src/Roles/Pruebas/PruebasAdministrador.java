67873fc5de0b7721677b7e6130d1a8c61c185e71
Sebas-1111 <js.martinezg1@uniandes.edu.co>
2026-05-02 14:16:18 -0500

    Import no usado en pruebas admin

--- Proyecto-1-DPOO/src/Roles/Pruebas/PruebasAdministrador.java
+++ Proyecto-1-DPOO/src/Roles/Pruebas/PruebasAdministrador.java
@@ -3,7 +3,6 @@ package Pruebas;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
