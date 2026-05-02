f404bcfdbac1a327c47d634732310a073637c757
Sebas-1111 <js.martinezg1@uniandes.edu.co>
2026-05-01 21:49:54 -0500

    Pruebas Solicitud Cambio Turno

--- Proyecto-1-DPOO/src/Roles/Pruebas/PruebasSolicitudCambioTurno.java
+++ Proyecto-1-DPOO/src/Roles/Pruebas/PruebasSolicitudCambioTurno.java
@@ -1,5 +1,51 @@
 package Pruebas;
 
-public class PruebasSolicitudCambioTurno {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import BoardGame.SolicitudCambioTurno;
+import BoardGame.Turno;
+import Roles.Cocinero;
+import Roles.Empleado;
+
+public class PruebasSolicitudCambioTurno {
+	
+	private Turno t1;
+	private Turno t2;
+	private Empleado e1;
+	private SolicitudCambioTurno sct1;
+	
+	@BeforeEach
+	public void setUp() {
+		t1 = new Turno("lunes", LocalTime.of(6, 0), LocalTime.of(20, 0));
+        t2 = new Turno("martes", LocalTime.of(6, 0), LocalTime.of(20, 0));
+		e1 = new Cocinero("Camiquin", "003", t1, "camiquinsita123@uniandes.edu.co", "lluvioso");
+		sct1 = new SolicitudCambioTurno(123, e1, t1, t2, "intercambio");
+	}
+	
+	@Test
+	@DisplayName("Test asignar otro empleado")
+	public void asignarOtroEmpleadoTest() {
+		sct1.asignarOtroEmpleado(e1);
+		assertEquals(e1, sct1.getOtroEmpleado(), "Otro empleado asignado incorrectamente");
+	}
+	
+	@Test
+	@DisplayName("Test aprobar")
+	public void aprobarTest() {
+		sct1.aprobar();
+		assertEquals("APROBADO", sct1.getEstado(), "Aprobado incorrectamente");
+	}
+	
+	@Test
+	@DisplayName("Test rechazar")
+	public void rechazarTest() {
+		sct1.rechazar();
+		assertEquals("RECHAZADO", sct1.getEstado(), "Rechazado incorrectamente");
+	}
 }
